141d0b1f3f8d85166bc19a28d6edaad3df4beb29
Vince Y <soda1015@foxmail.com>
2022-07-14 19:24:12 +0800

    1

--- src/main/java/me/hipoplar/zookeeper/monitor/ZkMonitorApplication.java
+++ src/main/java/me/hipoplar/zookeeper/monitor/ZkMonitorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZkMonitorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZkMonitorApplication.class, args);
 	}
